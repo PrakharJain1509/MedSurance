df94e53fa103b8f55fdbe8177f4f515887cfe8aa
prakharjain <prakharjain2004@gmail.com>
2025-03-01 22:29:06 +0530

    All codes Pushed (Phase-2)

---
+++ Final_frontend_avatar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MedicalExaminerPage from './MedicalChat';
+
+function App() {
+  return (
+    <div className="App">
+      <MedicalExaminerPage />
+    </div>
+  );
+}
+
+export default App;
